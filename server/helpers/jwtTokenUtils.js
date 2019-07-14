45bb4894998abf1e42e7cf97287c969b76941b67
joaquinto <roejoeodj12@gmail.com>
2019-07-14 18:34:29 +0100

    bug(implement feedback): Implement Feedback
    - Change req.headers.authorization to req.headers.token
    
    [Finishes #167279047]

--- server/helpers/jwtTokenUtils.js
+++ server/helpers/jwtTokenUtils.js
@@ -3,21 +3,17 @@ import jwt from 'jsonwebtoken';
 export default class JwtToken {
   static signToken(userId, userEmail, isAdmin) {
     const key = process.env.SECRET_KEY;
-    const token = `Bearer ${jwt.sign({ id: userId, email: userEmail, admin: isAdmin }, key, { expiresIn: '1h' })}`;
+    const token = jwt.sign({ id: userId, email: userEmail, admin: isAdmin }, key, { expiresIn: '1h' });
     return token;
   }
 
   static verifyToken(req, res, next) {
     const key = process.env.SECRET_KEY;
-    const tokenHeaders = req.headers.authorization;
+    const tokenHeaders = req.headers.token;
     if (!tokenHeaders) {
       return res.status(403).json({ status: 'error', error: 'No token provided' });
     }
-    const token = req.headers.authorization.split(' ')[1];
-    const options = {
-      expiresIn: '1h',
-    };
-    jwt.verify(token, key, options, (error, decoded) => {
+    jwt.verify(tokenHeaders, key, (error, decoded) => {
       if (error) {
         return res.status(401).json({ status: 'error', error: 'Invalid token provided' });
       }
